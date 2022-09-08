e961a47a028bd14aee01fb50748f109235796d39
KeyurMistry <keyurmist@gmail.com>
2022-09-08 14:07:21 +0100

    added connection message

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Roflsticks081994_",
+  database: "employee_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  connected();
+});
+
+connected = () => {
+  console.log("WELCOME TO THE EMPLOYEE MANAGER");
+  promptUser();
+};
