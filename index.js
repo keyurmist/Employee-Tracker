0a20450e1e51a5837b5d39db25cc792373405d79
KeyurMistry <keyurmist@gmail.com>
2022-09-08 23:34:03 +0100

    added function to view employees

--- index.js
+++ index.js
@@ -129,3 +129,24 @@ viewRoles = () => {
     promptUser();
   });
 };
+
+//function to view employees
+viewEmployees = () => {
+  const sql = `SELECT employee.id, 
+    employee.first_name, 
+    employee.last_name, 
+    role.title, 
+    department.name AS department,
+    role.salary, 
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+  connection.query(sql, (err, answer) => {
+    if (err) throw err;
+    console.table(answer);
+    promptUser();
+  });
+};
