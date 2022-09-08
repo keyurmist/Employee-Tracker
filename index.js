f4760b00b2a3dacc54c793d3f6f37bcb03b8599c
KeyurMistry <keyurmist@gmail.com>
2022-09-08 14:17:19 +0100

    added first prompt to initiate

--- index.js
+++ index.js
@@ -18,3 +18,90 @@ connected = () => {
   console.log("WELCOME TO THE EMPLOYEE MANAGER");
   promptUser();
 };
+
+//After connection user is prompted with the first question
+const promptUser = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choices",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Update an employee manager",
+          "View employees by department",
+          "Delete a department",
+          "Delete a role",
+          "Delete an employee",
+          "View department budgets",
+          "Quit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      const { choices } = answers;
+
+      if (choices === "View all departments") {
+        viewDepartments();
+      }
+
+      if (choices === "View all roles") {
+        viewRoles();
+      }
+
+      if (choices === "View all employees") {
+        viewEmployees();
+      }
+
+      if (choices === "Add a department") {
+        addDepartment();
+      }
+
+      if (choices === "Add a role") {
+        addRole();
+      }
+
+      if (choices === "Add an employee") {
+        addEmployee();
+      }
+
+      if (choices === "Update an employee role") {
+        updateEmployee();
+      }
+
+      if (choices === "Update an employee manager") {
+        updateManager();
+      }
+
+      if (choices === "View employees by department") {
+        employeeDepartment();
+      }
+
+      if (choices === "Delete a department") {
+        deleteDepartment();
+      }
+
+      if (choices === "Delete a role") {
+        deleteRole();
+      }
+
+      if (choices === "Delete an employee") {
+        deleteEmployee();
+      }
+
+      if (choices === "View department budgets") {
+        viewBudget();
+      }
+
+      if (choices === "Quit") {
+        connection.end();
+      }
+    });
+};
